86d3f355cffa669931c3f440ee1b1327011aa139
Vyacheslav <vmk_86@mail.ru>
2023-01-22 12:10:31 +0300

    save5

---
+++ Persister.java
@@ -0,0 +1,13 @@
+public class Persister implements IPersister {
+
+    private Persistable persistable;
+
+    public Persister(Persistable persistable) {
+        this.persistable = persistable;
+    }
+
+    @Override
+    public String save() {
+        return "Save persistable: " + persistable.save();
+    }
+}
